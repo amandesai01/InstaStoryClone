dc0d3416ed4e782c0d84ce0cac886bffc16d2d78
Aman Desai <amandesai01@gmail.com>
2020-06-21 15:41:16 +0530

    Router basic push

--- routes.js
+++ routes.js
@@ -1,9 +1,39 @@
-var router = require('express')()
-const bodyparser = require('body-parser')
+const router = require("express")();
+const bodyparser = require("body-parser");
+const fs = require("fs");
 
-const uploader = require('./upload-manager')
+const uploader = require("./upload-manager");
+const cloudinary = require("./cloudinary");
 
-router.use(bodyparser.urlencoded({ extended : false }))
-router.use(bodyparser.json())
+const getpath = async (path) => await cloudinary.uploads(path, "Images");
 
-module.exports = router
+router.use(bodyparser.urlencoded({ extended: false }));
+router.use(bodyparser.json());
+
+router.post("/createstory", uploader.array("image"), async (req, res) => {
+  try {
+    const urls = [];
+    const files = req.files;
+    const STORIES = 2;
+    var counter = 0;
+    for (const file in files) {
+      const { path } = file;
+      const newPath = await getpath(path);
+      urls.push(newPath);
+      fs.unlinkSync(path);
+      counter += 1;
+      if (counter >= STORIES) break;
+    }
+    res.status(200).json({
+      message: "Images Uploaded Successfully",
+      data: urls,
+    });
+  } catch (err) {
+    res.status(405).json({
+      message: err,
+      data: [],
+    });
+  }
+});
+
+module.exports = router;
