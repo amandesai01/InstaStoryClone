24d8ffd649408da27c142882f9c71fb18c700c9b
Aman Desai <amandesai01@gmail.com>
2020-06-21 15:40:46 +0530

    Cloudinary + Router Basic Version

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 var router = require('express')()
 const bodyparser = require('body-parser')
 
+const uploader = require('./upload-manager')
+
 router.use(bodyparser.urlencoded({ extended : false }))
 router.use(bodyparser.json())
 
