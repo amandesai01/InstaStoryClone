1ecdb9073ad4647fead9b3c320104f5c3a27a085
Aman Desai <amandesai01@gmail.com>
2020-06-21 16:29:37 +0530

    Revert "TWEAKS"
    This reverts commit 662b7e834f42ed4caecbde0a15a57366fb1b32cb.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const stories = require('./StoriesManager/routes')
+const routes = require('./routes')
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
-app.use(stories)
+app.use(routes)
 
 app.listen(PORT, () => {
     console.log("Listening to Port " + PORT)
