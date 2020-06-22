bf34b7cb84c97cc831bba26cf5a617535ac230b9
Aman Desai <amandesai01@gmail.com>
2020-06-22 22:15:49 +0530

    comments get method done

--- comment.js
+++ comment.js
@@ -17,12 +17,12 @@ router.post("/comment", (req, res) => {
     };
     mongo.connect(MONGODB_URL, (err, db) => {
       if (err) {
-        res.status(400).json({ message: "Internal Error!" });
+        res.status(400).json({ message: "Some Error!" });
         return;
       }
       db.collection("comments").insertOne(data, (err, result) => {
         if (err) {
-          res.status(400).json({ message: "Internal Error!" });
+          res.status(400).json({ message: "Some Error!" });
           return;
         }
         res.status(200).json({ message: "Posted!" });
@@ -30,8 +30,34 @@ router.post("/comment", (req, res) => {
     });
   } catch (error) {
       console.log(error);
-      res.status(400).json({ message: "Internal Error!" })
+      res.status(400).json({ message: "Some Error!" })
   }
 });
+
+router.get('/comment', (req, res) => {
+    rpostid = req.query.postid;
+    if(!rpostid){
+        res.status(400).json({ message: "Missing Argument - postid" });
+        return;
+    }
+    try {
+        mongo.connect(MONGODB_URL, (err, db) => {
+            if(err){
+                res.status(400).json({ message: "Some Error!" })
+                return;
+            }
+            console.log(rpostid)
+            db.collection('comments').find({ postid: rpostid }).toArray((err, result) => {
+                if(err){
+                    res.status(400).json({ message: "Some Error!" })
+                    return;
+                }
+                res.status(200).json(result)
+            })
+        })
+    } catch (error) {
+        
+    }
+})
 
 module.exports = router;
