9d2a22a4503ec8008903d14f05358c093eac2093
Aman Desai <amandesai01@gmail.com>
2020-06-22 11:14:42 +0530

    Basic Streaming Model

--- story.js
+++ story.js
@@ -6,13 +6,15 @@ const mongo = require("mongodb")
 const uploader = require("./upload-manager");
 const cloudinary = require("./cloudinary");
 
+const Story = require('./StoryModel')
+
 const getpath = async (path) => await cloudinary.uploads(path, "Images");
 const MONGODB_URL = "mongodb://localhost:27017/insta"
 
 router.use(bodyparser.urlencoded({ extended: false }));
 router.use(bodyparser.json());
 
-router.post("/createstory", uploader.array("image"), async (req, res) => {
+router.post("/story", uploader.array("image"), async (req, res) => {
   try {
     const urls = [];
     const files = req.files;
@@ -51,5 +53,30 @@ router.post("/createstory", uploader.array("image"), async (req, res) => {
     });
   }
 });
+
+router.get('/story', (req, res) => {
+  const page = parseInt(req.body.page);
+  const limit = parseInt(req.body.limit);
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = (page) * limit;
+
+  const results = {}
+
+  if(startIndex > 0){
+    results.previous = {
+      page: page - 1,
+      limit: limit
+    }
+  }
+  if(endIndex < SOMELENGTH){
+    results.next = {
+      page: page + 1,
+      limit: limit
+    }
+  }
+  
+
+})
 
 module.exports = router;
