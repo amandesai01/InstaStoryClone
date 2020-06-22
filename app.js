8bf0226b1dd148d9e0b688ea0b75c75784177752
Aman Desai <amandesai01@gmail.com>
2020-06-22 21:45:11 +0530

    comments integrates

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const story = require('./story')
+const comment = require('./comment')
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
 app.use(story)
+app.use(comment)
 
 app.listen(PORT, () => {
     console.log("Listening to Port " + PORT)
